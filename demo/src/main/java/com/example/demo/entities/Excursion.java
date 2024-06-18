533ab033956431454f203455b1f3d15fac8afcd4
= <seankeane93@gmail.com>
2024-06-17 23:15:05 -0700

    WIP added more debugging tools to solve for posting/getting errors.  Solved for images not posting in built application.  Rebuilt Maven to continue work on local machine instead of VM.

--- demo/src/main/java/com/example/demo/entities/Excursion.java
+++ demo/src/main/java/com/example/demo/entities/Excursion.java
@@ -1,5 +1,6 @@
 package com.example.demo.entities;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -19,29 +20,37 @@ public class Excursion {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "excursion_id", nullable = false, updatable = false)
+    @JsonProperty("id")
     private Long id;
 
     @Column(name = "excursion_title")
+    @JsonProperty("excursion_title")
     private String excursionTitle;
 
     @Column(name = "excursion_price")
+    @JsonProperty("excursion_price")
     private BigDecimal excursionPrice;
 
     @Column(name = "image_url")
+    @JsonProperty("image_URL")
     private String imageUrl;
 
     @CreationTimestamp
     @Column(name = "create_date", updatable = false)
+    @JsonProperty("create_date")
     private Date createDate;
 
     @UpdateTimestamp
     @Column(name = "last_update")
+    @JsonProperty("last_update")
     private Date lastUpdate;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "vacation_id")
+    @JsonProperty("vacation")
     private Vacation vacation;
 
     @ManyToMany(mappedBy = "excursions")
+    @JsonProperty("cart_items")
     private Set<CartItem> cartItems;
 }
--- demo/src/main/java/com/example/demo/entities/Vacation.java
+++ demo/src/main/java/com/example/demo/entities/Vacation.java
@@ -1,7 +1,9 @@
 package com.example.demo.entities;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
@@ -14,23 +16,28 @@ import java.util.Set;
 @Table(name = "vacations")
 @Getter
 @Setter
+@NoArgsConstructor
 public class Vacation {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "vacation_id")
+    @JsonProperty("id")
     private Long id;
 
     @Column(name = "vacation_title")
+    @JsonProperty("vacation_title")
     private String vacationTitle;
 
     @Column(name = "image_url")
+    @JsonProperty("image_URL")
     private String imageUrl;
 
     @Column(name = "description")
     private String description;
 
     @Column(name = "travel_fare_price")
+    @JsonProperty("travel_price")
     private BigDecimal travelPrice;
 
     @CreationTimestamp
